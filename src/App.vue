bdbb4750e3bce78910e1fc4f527026dcf02a3d0e
Liu Yuhe <171144077+liuyuhe666@users.noreply.github.com>
2025-05-12 20:10:41 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import projects from './assets/projects.json'
+import Project from './components/Project.vue'
+</script>
+
+<template>
+  <div class="max-w-3xl mx-auto my-12 px-7 lg:px-0">
+    <h1 class="text-2xl font-bold tracking-tight text-neutral-900 dark:text-neutral-100 sm:text-3xl lg:text-4xl">
+      My Projects
+    </h1>
+    <p class="mt-3 text-sm leading-6 text-neutral-600 dark:text-neutral-400 sm:mt-4 lg:mt-6 sm:leading-7 lg:leading-8 sm:text-base lg:text-lg">
+      Here are some of the current projects I've been working on. I really enjoy creating new projects and coming up with new ideas. I'm always working on something new, so check back often!
+    </p>
+    <div
+      class="grid items-stretch w-full grid-cols-1 my-8 gap-7 sm:gap-5 sm:grid-cols-2"
+    >
+      <Project
+        v-for="item in projects"
+        :key="item.name"
+        :name="item.name"
+        :description="item.description"
+        :url="item.url"
+        :image="item.image"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
